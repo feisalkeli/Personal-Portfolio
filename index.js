463d8ce8227ef9422f640c6bb9da8e3b2b6614b9
Feisal Migo Keli <kellyfeisal2000@gmail.com>
2023-03-12 16:20:40 +0300

    Seccond Commit

--- index.js
+++
@@ -1,8 +0,0 @@
-const about = document.getElementById("About-me");
-const projects = document.getElementById("Projects");
-const skills = document.getElementById("Skills");
-const contact = document.getElementById("Contact-me");
-
-about.addEventListener("click", function () {
-  document.getElementById("Projects").style.display = "none";
-});
